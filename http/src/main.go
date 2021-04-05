f2bf85a6a00eae55701bfaab1e02cab3e4b93f7f
fabio.lonegro <fabio.lonegro@deltatre.com>
2021-04-05 13:34:18 +0200

    added videos functionalities

--- http/src/main.go
+++ http/src/main.go
@@ -3,5 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world")
+	videos := getVideos()
+
+	fmt.Println(videos)
 }
---
+++ http/src/videos.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+)
+
+type video struct {
+	Id          string
+	Title       string
+	Description string
+}
+
+func getVideos() (videos []video) {
+	fileBytes, err := ioutil.ReadFile("./videos.json")
+
+	if err != nil {
+		panic(err)
+	}
+
+	err = json.Unmarshal(fileBytes, &videos)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return videos
+}
+
+func saveVideos(videos []video) {
+	videoBytes, err := json.Marshal(videos)
+
+	if err != nil {
+		panic(err)
+	}
+
+	ioutil.WriteFile("./videos-updated.json", videoBytes, 0644)
+
+	if err != nil {
+		panic(err)
+	}
+}
