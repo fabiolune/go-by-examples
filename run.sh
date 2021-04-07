cc3a367ffa0aba0b4cb9ca79bd93db845532d3b3
fabio.lonegro <fabio.lonegro@deltatre.com>
2021-04-07 17:46:43 +0200

    fixed run scripts to be able to work with non http apps

--- run.sh
+++ run.sh
@@ -2,4 +2,12 @@
 docker container stop go-$1-run-ex 2>/dev/null
 docker container rm go-$1-run-ex 2>/dev/null;
 docker build ./$1 -t go-$1-run 
-docker run -d --rm -p 8080:8080 --name go-$1-run-ex go-$1-run
+
+if [ ! -z "$2" ] && [ $2 = "expose" ]
+then
+	echo docker run -d --rm -p $3:$3 --name go-$1-run-ex go-$1-run
+	docker run -d --rm -p $3:$3 --name go-$1-run-ex go-$1-run
+else
+	echo docker run --rm --name go-$1-run-ex go-$1-run
+	docker run --rm --name go-$1-run-ex go-$1-run
+fi
--- watchrun.sh
+++ watchrun.sh
@@ -1,7 +1,7 @@
-./run.sh $1
+./run.sh $1 $2 $3
 while true; do
 
   inotifywait -q -e modify,create,delete,close -r $1 && \
-  ./run.sh $1
+  ./run.sh $1 $2 $3
 
 done
