9e356ed8378736d3ae1221bce615431d452bb74c
fabio.lonegro <fabio.lonegro@deltatre.com>
2021-04-10 17:08:33 +0200

    context to introduce canellation signals and timeouts

---
+++ ctx/README.md
@@ -0,0 +1,5 @@
+# Introduction
+
+This project is about the `context.Context` availabe in the standard library.
+
+More info [here](https://www.youtube.com/watch?v=GYtZsyT4gKo)
--- ctx/src/main.go
+++ ctx/src/main.go
@@ -1,8 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"math/rand"
+	"time"
+)
 
-func main(){
-	fmt.Println("Hello world from ctx")
+const (
+	minLatency  = 10
+	maxLatency  = 5000
+	maxDuration = 2500
+)
+
+func main() {
+	rootCtx := context.Background()
+
+	for i := 0; i < 50; i++ {
+		invokeCancellableGetData(rootCtx)
+	}
+
 }
 
+func invokeCancellableGetData(ctx context.Context) {
+	ctx, cancel := context.WithTimeout(ctx, time.Duration(maxDuration)*time.Millisecond)
+	defer cancel()
+	fmt.Println(getDataCtxAware(ctx))
+}
+
+func getDataCtxAware(ctx context.Context) (string, error) {
+	res := make(chan string)
+	go func() {
+		res <- getData()
+		close(res)
+	}()
+
+	for {
+		select {
+		case dst := <-res:
+			return dst, nil
+		case <-ctx.Done():
+			return "", ctx.Err()
+		}
+	}
+}
+
+// slow function simultaing some IO operation
+func getData() string {
+	rand.Seed(time.Now().Unix())
+	latency := minLatency + rand.Intn(maxLatency-minLatency)
+	time.Sleep(time.Millisecond * time.Duration(latency))
+	return fmt.Sprintf("result with latency %v", latency)
+}
